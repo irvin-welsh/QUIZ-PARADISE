fdbdff2ea5a459b4c4f8298c8c18cbd6a8095ff0
irvin-welsh <99cryptoman@gmail.com>
2020-04-26 12:40:40 +0300

    added error handling when using main menu

--- quiz_game.py
+++ quiz_game.py
@@ -9,10 +9,11 @@ quiz_capitals={
 }
 
 def geo_exam():
-    # get list of Q&A
-    questions=list(quiz_capitals.items())
+    student_name=input("Enter your name: ")
+    questions=list(quiz_capitals.items()) # get list of Q&A
     random.shuffle(questions)
     # set exam score to 0 (zero)
+    attempt=0
     exam_score=0
     mistakes=0
     # loop through questions
@@ -32,10 +33,28 @@ def geo_exam():
         print("You failed an exam!")
     else:
         print("Congratulations, you've passed an exam!")
+    attempt+=1
+    print(student_name,", you've made", attempt,"attempt(s) and got", exam_score, "points.")
+
+def help_page():
+    print("\n\t=== Instruction on how to use QUIZ PARADISE ===\n* Each quiz has 10 questions dedicated to 1 scientific area;\n** Correct answer gives you 1 point;\n*** To successfully pass quiz you must earn 8 or more points;")
+    print("**** There is no time limits to finish quizes;\n***** To exit QUIZ PARADISE at any stage press Ctrl+C;\n\n")
+    go_to_menu=input("If you would like to continue to the main menu, type: menu or exit to quit the Master: ").lower()
+    if go_to_menu=="menu":
+        return main_menu()
+    elif go_to_menu=="exit":
+        quit("Shutting down Quiz Master. Bye!")
+    else:
+        quit("Wrong choice. Shutting down Quiz Master. Bye!")
 
 def main_menu():
-    print("##################################\nWelcome to QUIZ PARADISE\n##################################\nPlease select an option:\n1. Take GEO Quiz\n2. Take MATH Quiz\n3. Quit\n##################################")
-    choice=int(input("Enter your choice: "))
+    print("\n##################################\nWelcome to QUIZ PARADISE\n##################################\nPlease select an option:\n1. Take GEO Quiz\n2. Take MATH Quiz\n3. Help page\n4. Quit\n##################################")
+    try:
+        choice=int(input("Enter your choice: "))
+        pass
+    except (UnboundLocalError,ValueError):
+        print("\nOoops! Your choice was wrong. Made another one \n\u2193 \u2193 \u2193")
+        return main_menu()
     if choice==1:
         print("Welcome to the GEO quiz. To pass you need to have 8 points or higher. 1 question = 1 point. Let's start")
         geo_exam()
@@ -45,5 +64,15 @@ def main_menu():
         if choice2==1:
             print("Welcome to the GEO quiz. To pass you need to have 8 points or higher. 1 question = 1 point. Let's start")
             geo_exam()
-        else: quit()
+    elif choice==3:
+        return help_page()
+    elif choice in range(5,10):
+        print("There is no such option. Would you like to make another choice?")
+        choice3=input("Type yes to continue to Main Menu or no to quit: ").lower()
+        if choice3=="yes":
+            return main_menu()
+        else:
+            quit("Shutting down Quiz Master. Bye!")
+    else:
+        quit("Shutting down Quiz Master. Bye!")
 main_menu()
